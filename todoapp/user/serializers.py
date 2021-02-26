3c20fd3928107fceea29b3f830fcb98f5c75e692
Ivan Chan <ivanchancls421@gmail.com>
2021-02-26 21:55:15 +0800

    modified the password style

--- todoapp/user/serializers.py
+++ todoapp/user/serializers.py
@@ -5,15 +5,16 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
 
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+        write_only=True,
+        min_length=5
+    )
+
     class Meta:
         model = get_user_model()
         fields = ('username', 'password',)
-        extra_kwargs = {
-            'password': {
-                'write_only': True,
-                'min_length': 5,
-            }
-        }
 
     def create(self, validated_data):
         return get_user_model().objects.create_user(**validated_data)
